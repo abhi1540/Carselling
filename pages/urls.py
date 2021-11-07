09f9c766b3b27cbaf20f71cc660ada33c8dcc99d
abhisek <abhisek1651990@gmail.com>
2021-11-07 13:31:05 +0530

    added pages app

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 
-from .views import Home
+from .views import Home, About, Services, Contact
 urlpatterns = [
-    path('', Home.as_view(), name='home')
+    path('', Home.as_view(), name='home'),
+    path('about', About.as_view(), name='about'),
+    path('services', Services.as_view(), name='services'),
+    path('contact', Contact.as_view(), name='contact'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -7,3 +7,22 @@ class Home(View):
     template_name = 'home.html'
     def get(self, request):
         return render(request, self.template_name)
+
+
+
+class About(View):
+    template_name = 'about.html'
+    def get(self, request):
+        return render(request, self.template_name)
+
+
+class Services(View):
+    template_name = 'services.html'
+    def get(self, request):
+        return render(request, self.template_name)
+
+
+class Contact(View):
+    template_name = 'contact.html'
+    def get(self, request):
+        return render(request, self.template_name)
